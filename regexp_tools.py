054474d2b4d1461b60eea2d34cca3ef49c0cf4ee
Michael Blahay <mblahay@gmail.com>
2019-08-14 10:42:39 -0400

    Moving the re import out of function. Adding to comments for regexp_like

--- regexp_tools.py
+++ regexp_tools.py
@@ -1,11 +1,18 @@
+from re import search
+
 def regexp_like(args, argp):
     '''
-    A simply simple wrapper around the re.search function. The result
+    A simple wrapper around the re.search function. The result
     of that function will be converted to a binary True/False.
     
     This function will return a simple True or False, no match object.
+
+    Parameters:
+    -----------
+    args - This is the string argument which is to be searched.
+
+    argp - This is the pattern that is used when searching args
     '''
-    from re import search 
     return bool(search(argp, args)) # Simply execute the search method using the string and pattern, 
                                     # then interpret the existance of a returned match object into 
                                     # a True of False using the bool constructor.
